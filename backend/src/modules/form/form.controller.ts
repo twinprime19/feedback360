3d7c70e871baa88927189b703ba448b2adcd640c
Nguyen Duc Tai <ductai2982@gmail.com>
2024-09-26 15:38:45 +0700

    update

--- backend/src/modules/form/form.controller.ts
+++ backend/src/modules/form/form.controller.ts
@@ -133,7 +133,7 @@ export class FormController {
 
   // export result statistic of form
   @Get("/statistic/:id")
-  @UseGuards(AdminOnlyGuard, PoliciesGuard)
+  // @UseGuards(AdminOnlyGuard, PoliciesGuard)
   async downloadPdf(@Param("id") formID: string, @Res() res: Response) {
     const { filename, buffer } = await this.formService.generatePdfFile(formID);
 
